6fd4c6454c31d5181bfbb9f17bdbd422cb071dfe
Michele Dolfi <dol@zurich.ibm.com>
2024-10-02 09:47:54 +0200

    use LOCATION strategy
    Signed-off-by: Michele Dolfi <dol@zurich.ibm.com>

--- build.py
+++ build.py
@@ -46,7 +46,7 @@ def build_local(num_threads: int):
         "cmake",
         "-B", f"{BUILD_DIR}",
         f"-DPython3_ROOT_DIR={sys.exec_prefix}",
-        # f"-DPython3_FIND_STRATEGY=LOCATION",
+        f"-DPython3_FIND_STRATEGY=LOCATION",
     ]
     config_cmd.extend(get_pybind11_cmake_args())
     success = run(config_cmd, cwd=ROOT_DIR)
